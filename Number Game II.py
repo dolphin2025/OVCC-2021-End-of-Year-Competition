f04603dc8f5e310a13361105fd3f071bbb66fe81
dolphin2025 <57335293+dolphin2025@users.noreply.github.com>
2022-04-01 07:18:33 -0700

    Update Number Game II.py

--- Number Game II.py
+++ Number Game II.py
@@ -1,5 +1,2 @@
 n = int(input())
-for i in range(n + 1, n + 9):
-    if i % 2 and not all(i % d for d in range(2, int(i**.5)+1)):
-        print(i)
-        break
+print([i for i in range(n+1,n+9) if i%2 and not all(i%d for d in range(2,int(i**.5)+1))][0])
