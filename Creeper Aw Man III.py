8c4ed25e76f59ffdf64079d96f8fcd35ae3e6edd
dolphin2025 <57335293+dolphin2025@users.noreply.github.com>
2022-04-01 07:14:58 -0700

    Update Creeper Aw Man III.py

--- Creeper Aw Man III.py
+++ Creeper Aw Man III.py
@@ -1,21 +1,3 @@
-#
-# import random
-#
-# n = 1000
-# grid = [[['O','C'][random.randint(1,10)==1] for j in range(n)] for i in range(n)]
-#
-# # #############################################################
-#
-# import sys
-# sys.stdin = open('test.txt')
-# sys.stdout = open('test.txt','w')
-# print(n)
-# for i in grid:
-#     print(''.join([str(j) for j in i]))
-# sys.stdout = open('out.txt','w')
-
-##############################################################
-
 import sys; sys.setrecursionlimit(10**7)
 n = int(input()); grid = [list(input()) for i in range(n)]
 
